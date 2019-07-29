dd687963fd35a0deb6166195ad00753d8ab5de72
servetcos <servet.edu@gmail.com>
2019-07-30 01:56:48 +0200

    Working with Jinja templates

--- app/views.py
+++ app/views.py
@@ -2,6 +2,12 @@ from app import app
 
 from flask import render_template
 
+from datetime import datetime
+
+@app.template_filter("clean_date")
+def clean_date(dt):
+    return dt.strftime("%d %b %Y")
+
 @app.route('/') # decorator('/) function will "fire" index() when entering this url
 def index():
     return render_template("public/index.html")
@@ -11,7 +17,7 @@ def jinja():
 
     my_name = "Servet"
 
-    age = 28
+    age = 27
 
     langs = ["Python", "C++", "Bash", "C", "MATLAB"]
 
@@ -38,12 +44,26 @@ def jinja():
         def clone(self):
             return f"Cloning into {self.url}"
 
+    my_remote = GitRemote(
+        name="Flask Jinja",
+        description="Template design tutorial",
+        url="https://github.com/servetcoskun/jinja.git"
+    )
+
     def repeat(x, qty):
         return x * qty
 
+    date = datetime.utcnow()
+
+    my_html = "<h2> This is some HTML</h2>"
+
+    suspicious = "<script>alert('You got hacked!')</script>"
+
     return render_template('public/jinja.html', my_name=my_name, age=age,
     langs=langs, friends=friends, colours=colours,
-    cool=cool, GitRemote=GitRemote, repeat=repeat)
+    cool=cool, GitRemote=GitRemote, repeat=repeat,
+    my_remote=my_remote, date=date, my_html=my_html,
+    suspicious=suspicious)
 
 @app.route('/about')
 def about():
