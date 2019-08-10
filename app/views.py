84ac011bbfbc4b3c52ecb872491dfbacf6f2de56
servetcos <servet.edu@gmail.com>
2019-08-11 01:35:11 +0200

    Uploading files with Flask

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 import os
 
+from werkzeug.utils import secure_filename
+
 @app.template_filter("clean_date")
 def clean_date(dt):
     return dt.strftime("%d %b %Y")
@@ -193,20 +195,52 @@ def query():
 
 # put in config file later
 app.config["IMAGE_UPLOADS"] = "/Users/servetcoskun/app/app/static/img/uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 
 
+def allowed_image(filename):
 
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+def allowed_image_filesize(filesize):
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+ 
 @app.route("/upload-image", methods=["GET","POST"])
 def upload_image():
-
     if request.method == "POST":
 
         if request.files:
-
+            if not allowed_image_filesize(request.cookies.get("filesize")):
+                print("File exceeded maximum size")
+                return redirect(request.url)
+            
             # file storage object
             image = request.files["image"]
 
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            if image.filename == "":
+                print("Image must have a filename")
+                return redirect(request.url)
+            
+            if not allowed_image(image.filename):
+                print("That image extension is not allowed")
+                return redirect(request.url)
+
+            else:
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
 
             print("Image saved")
 
